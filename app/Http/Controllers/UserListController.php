bd93b3e443768a8ceb7a38db26476818009a5007
LesHank <henrik.berglund@gmail.com>
2022-04-08 13:11:15 +0200

    add userlist table

---
+++ app/Http/Controllers/UserListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserListController extends Controller
+{
+    //
+}
+
+// getlist from specific user_io
+
+// Create list
+
+// deleteList
+
+
